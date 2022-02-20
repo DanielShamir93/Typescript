cf250126a456628b317a4229b1855361bdc441f4
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-02-20 22:31:47 +0200

    objects annotation

--- annotation/functions.ts
+++ annotation/functions.ts
@@ -17,7 +17,7 @@ const forecast = {
   weather: 'sunny'
 }
 
-const logWeather = ({ date, weather }: { date: Date, weather: string }): void => {
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
   console.log(date);
   console.log(weather);
 }
